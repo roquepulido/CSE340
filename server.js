a892df27092a910de0018901e2f4f88687cdb9dd
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2025-05-24 18:22:21 -0600

    ADD Week3 Learning Activity 1

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
+
 
 /* ***********************
  * View Engine and Templates
@@ -22,7 +25,8 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-app.get("/", (req, res) => {res.render("index", { title: "CSE Motors" });});
+app.get("/", baseController.buildHome);
+app.use("/inv", inventoryRoute);
 
 /* ***********************
  * Local Server Information
