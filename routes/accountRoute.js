d30992c62d14a91a40cd1260e40b2b7b7a82553b
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2025-05-31 11:23:01 -0600

    ADD w4.2.2 Server-Side Data Validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,11 +3,10 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities/");
+const regValidate = require("../utilities/account-validation");
 
 // Route to build inventory by classification view
-router.get("/login",
-  utilities.handleErrors(accountController.buildLogin)
-);
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 router.get(
   "/register",
@@ -16,6 +15,8 @@ router.get(
 
 router.post(
   "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
   utilities.handleErrors(accountController.registerAccount)
 );
 
